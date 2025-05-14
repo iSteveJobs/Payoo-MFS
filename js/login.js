1797e690b6603469a183b5635d2ddde07e1bee53
iSteveJobs <iubat.masumbillah@gmil.com>
2025-05-14 20:26:51 +0600

    upto login page done

--- js/login.js
+++ js/login.js
@@ -3,15 +3,14 @@
 document.getElementById('btn-login').addEventListener('click', function(event){
     event.preventDefault();
 
-    const inputNumber = document.getElementById('input-number').value;
+    const enterNumber = document.getElementById('enter-number').value;
+    const enterPin = document.getElementById('enter-pin').value;
 
-    const inputPin = document.getElementById('input-pin').value;
-    
-    if(inputNumber === '5' && inputPin === '5555'){
-        console.log('you are logged in');
-        window.location.href = '/home.html';
+    if(enterNumber === '55' && enterPin === '5555'){
+        console.log('Login Successful');
+        window.location.href = '/home.html'
     }
     else{
-        alert('phone number or pin not matched')
+        alert('Wrong Number or Pin, Try again!');
     }
 })
